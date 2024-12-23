9e1a11d5078ec086705330d2687bde9c3a17ec8f
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2024-12-23 13:37:09 +0100

    Update .config/nvim/lua/plugins/example.lua

--- dot_config/nvim/lua/plugins/example.lua
+++ dot_config/nvim/lua/plugins/example.lua
@@ -6,9 +6,34 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim", config = true, opts = {
-    transparent_mode = true,
-  } },
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = true,
+    opts = {
+      transparent_mode = true,
+      terminal_colors = true,
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines, and errors
+      contrast = "hard", -- can be "hard", "soft", or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
