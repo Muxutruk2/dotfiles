df33128f50e30acf840a82824eac641f763f94b1
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-01-27 17:45:21 +0100

    Wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,61 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Gruvbox Dark (Gogh)"
+-- Window
 
--- and finally, return the configuration to wezterm
+local mux = wezterm.mux
+
+wezterm.on("gui-attached", function(_)
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
+
+-- Program
+config.default_prog = { "/usr/bin/fish" }
+
+-- Font
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+
+-- Color Scheme
+config.color_scheme = "Gruvbox Dark (Gogh)"
+config.window_background_gradient = {
+	orientation = "Vertical",
+	colors = {
+		"#282828",
+		"#282828",
+		"#3C3836",
+	},
+	interpolation = "Linear",
+	blend = "Rgb",
+}
+-- Lauch Menu
+local launch_menu = {}
+
+table.insert(launch_menu, {
+	label = "TMUX",
+	args = { "tmux", "new", "-As0" },
+})
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "powershell.exe", "-NoLogo" },
+	})
+end
+
+config.launch_menu = launch_menu
+
+-- Tab Bar
+config.enable_tab_bar = false
+
+-- Keybindings
+config.keys = {
+	{ key = "a", mods = "ALT", action = wezterm.action.ShowLauncher },
+}
+
 return config
