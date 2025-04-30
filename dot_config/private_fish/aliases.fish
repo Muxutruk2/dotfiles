1c97d924ab79f9e72304726c3224525b3324f3b6
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-04-30 10:37:24 +0200

    Fish set manpager and catppuccin plugin

--- dot_config/private_fish/aliases.fish
+++ dot_config/private_fish/aliases.fish
@@ -44,6 +44,8 @@ if command_exists bat
     and not is_no_de
     alias cat='bat'
     alias ccat='command cat'
+    set -x MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+
 end
 
 if command_exists tree
