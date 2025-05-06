3753b3fa2103fe04c8c774b1a799df470051f892
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-05-06 11:16:36 +0200

    Fish: Somethings

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -46,6 +46,5 @@ set -x GTK_THEME "catppuccin-mocha-sapphire-standard+default"
 silent_add_path "$HOME/.cargo/bin" "$HOME/.local/bin" "$HOME/.config/emacs/bin"
 if_command_run pyenv 'pyenv init - | source'
 if_command_run fish_hybrid_key_bindings fish_hybrid_key_bindings
-# if_command_run tide "tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='One line' --prompt_spacing=Sparse --icons='Few icons' --transient=Yes"
 source_if_exists "$HOME/.cargo/env.fish"
 git_clone_if_not_exists "$HOME/.tmux/plugins/tpm/tpm" "https://github.com/tmux-plugins/tpm"
