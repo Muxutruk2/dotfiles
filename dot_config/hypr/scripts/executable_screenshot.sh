85bccbc05a69219405945ca1a20d4a2bf189b32c
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-06-27 11:53:07 +0200

    Hyprland: Dump (only for PC)

--- dot_config/hypr/scripts/executable_screenshot.sh
+++ dot_config/hypr/scripts/executable_screenshot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export PATH="$HOME/.nix-profile/bin:/run/current-system/sw/bin:$PATH"
+
+XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
+
 filename="$XDG_PICTURES_DIR/Screenshots/$(date +'%Y-%m-%d_%H-%M-%S').png"
 area=$(slurp)
 
